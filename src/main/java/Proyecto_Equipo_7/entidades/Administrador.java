c4596a2e68d1882899878725412cc818a7d19672
Ezequiel <ezequielarce_1996@hotmail.com>
2023-11-21 14:50:52 -0300

    entidades con uso de lombok

--- src/main/java/Proyecto_Equipo_7/entidades/Administrador.java
+++ src/main/java/Proyecto_Equipo_7/entidades/Administrador.java
@@ -1,80 +1,16 @@
 package Proyecto_Equipo_7.entidades;
 
-import Proyecto_Equipo_7.enumeradores.Rol;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import org.hibernate.annotations.GenericGenerator;
 
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Administrador extends Usuario {
-
-    
-     @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
-    private String id;
-
-    public Administrador() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDomicilio() {
-        return domicilio;
-    }
-
-    public void setDomicilio(String domicilio) {
-        this.domicilio = domicilio;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Rol getRol() {
-        return rol;
-    }
-
-    public void setRol(Rol rol) {
-        this.rol = rol;
-    }
-     
-     
+   
 }
--- src/main/java/Proyecto_Equipo_7/entidades/Proveedor.java
+++ src/main/java/Proyecto_Equipo_7/entidades/Proveedor.java
@@ -2,6 +2,11 @@ package Proyecto_Equipo_7.entidades;
 
 import Proyecto_Equipo_7.enumeradores.Rol;
 import Proyecto_Equipo_7.enumeradores.Servicio;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -12,6 +17,10 @@ import javax.persistence.PrimaryKeyJoinColumn;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @PrimaryKeyJoinColumn(referencedColumnName = "id")
 public class Proveedor {
 
@@ -36,88 +45,5 @@ public class Proveedor {
 
     @Enumerated(EnumType.STRING)
     private Servicio servicio;
-
-    public Proveedor() {
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDomicilio() {
-        return domicilio;
-    }
-
-    public void setDomicilio(String domicilio) {
-        this.domicilio = domicilio;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Rol getRol() {
-        return rol;
-    }
-
-    public void setRol(Rol rol) {
-        this.rol = rol;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Integer getHonorario() {
-        return honorario;
-    }
-
-    public void setHonorario(Integer honorario) {
-        this.honorario = honorario;
-    }
-
-    public Imagen getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(Imagen imagen) {
-        this.imagen = imagen;
-    }
-
-    public Servicio getServicio() {
-        return servicio;
-    }
-
-    public void setServicio(Servicio servicio) {
-        this.servicio = servicio;
-    }
 
 }
