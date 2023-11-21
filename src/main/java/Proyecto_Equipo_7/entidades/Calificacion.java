5f46a03dda0b8c511f062406eda80c8e375badd4
Exequiel <exee.lp@hotmail.com>
2023-11-21 18:06:43 -0300

    Calificacion

--- src/main/java/Proyecto_Equipo_7/entidades/Calificacion.java
+++ src/main/java/Proyecto_Equipo_7/entidades/Calificacion.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Proyecto_Equipo_7.entidades;
 
 import javax.persistence.Entity;
@@ -5,37 +9,37 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.Table;
+import javax.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
+/**
+ *
+ * @author Usuario
+ */
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Trabajo {
+public class Calificacion {
 
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
+    
+    @OneToOne
+    private Trabajo Trabajo;
 
-    @ManyToOne
-    @JoinColumn(name = "usuario_id")
-    private Usuario usuario;
-
-    @ManyToOne
-    @JoinColumn(name = "proveedor_id")
+    @OneToOne
     private Proveedor proveedor;
 
-    private Calificacion calificacion;
+    private Integer calificacion;
 
-    private boolean terminado;
-
+    private String comentario;
 
 }
