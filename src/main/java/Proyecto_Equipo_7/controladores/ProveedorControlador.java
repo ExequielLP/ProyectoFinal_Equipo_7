7c32f1b74609d03c828a26710d39085d028b2a30
Ezequiel <ezequielarce_1996@hotmail.com>
2023-11-27 16:37:13 -0300

    Añadi metodo eliminar() al controlador proveedor

--- src/main/java/Proyecto_Equipo_7/controladores/ProveedorControlador.java
+++ src/main/java/Proyecto_Equipo_7/controladores/ProveedorControlador.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,7 +49,16 @@ public class ProveedorControlador {
 
             return "registro.html";
         }
+    }
 
+    @PostMapping("eliminarProveedor/{id}")
+    public String eliminarProveedor(@PathVariable String id, ModelMap modelo){
+        try {
+            proveedorservicio.eliminar(id);
+        } catch (MiException ex){
+            modelo.put("error", ex.getMessage());
+        }
+        return "redirect:/proveedor/listarProveedor";
     }
 
 }
