b82a81d1740342c147f2ada2fe4fc9d78d66f287
christian2711 <distribuidoracalamuchita@gmail.com>
2023-12-11 19:07:27 -0300

    w

--- src/main/java/Proyecto_Equipo_7/repositorios/ProveedorRepositorio.java
+++ src/main/java/Proyecto_Equipo_7/repositorios/ProveedorRepositorio.java
@@ -1,6 +1,6 @@
 package Proyecto_Equipo_7.repositorios;
 
-import java.awt.print.Pageable;
+
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
